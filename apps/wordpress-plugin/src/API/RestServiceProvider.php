177c4a08162eedddf27f0b52c72b11643e59b4b7
design-by-greg <contact@designbygreg.fr>
2026-06-29 11:21:33 +0200

    Add REST service provider

---
+++ apps/wordpress-plugin/src/API/RestServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DBGPlatform\API;
+
+use DBGPlatform\API\Routes\IdentityRoutes;
+use DBGPlatform\API\Routes\ProjectRoutes;
+use DBGPlatform\API\Routes\AssetRoutes;
+use DBGPlatform\API\Routes\CommerceRoutes;
+
+class RestServiceProvider
+{
+    public function register(): void
+    {
+        add_action('rest_api_init', [$this, 'registerRoutes']);
+    }
+
+    public function registerRoutes(): void
+    {
+        (new IdentityRoutes())->register();
+        (new ProjectRoutes())->register();
+        (new AssetRoutes())->register();
+        (new CommerceRoutes())->register();
+    }
+}
