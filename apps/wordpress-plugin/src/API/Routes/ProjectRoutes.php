3a27b7be941e8995174b2e26d7b3766e75d337b0
design-by-greg <contact@designbygreg.fr>
2026-06-29 12:18:00 +0200

    Connect Project routes to repository

--- apps/wordpress-plugin/src/API/Routes/ProjectRoutes.php
+++ apps/wordpress-plugin/src/API/Routes/ProjectRoutes.php
@@ -2,25 +2,49 @@
 
 namespace DBGPlatform\API\Routes;
 
+use DBGPlatform\Database\Repositories\ProjectRepository;
 use WP_REST_Request;
 use WP_REST_Response;
 
 class ProjectRoutes
 {
+    private ProjectRepository $projects;
+
+    public function __construct()
+    {
+        $this->projects = new ProjectRepository();
+    }
+
     public function register(): void
     {
         register_rest_route('dbg/v1', '/projects', [
-            'methods' => 'GET',
-            'callback' => [$this, 'listProjects'],
-            'permission_callback' => '__return_true',
+            [
+                'methods' => 'GET',
+                'callback' => [$this, 'listProjects'],
+                'permission_callback' => '__return_true',
+            ],
+            [
+                'methods' => 'POST',
+                'callback' => [$this, 'createProject'],
+                'permission_callback' => '__return_true',
+            ],
         ]);
     }
 
     public function listProjects(WP_REST_Request $request): WP_REST_Response
     {
         return new WP_REST_Response([
-            'data' => [],
-            'message' => 'Project API scaffold',
+            'data' => $this->projects->all(),
         ], 200);
+    }
+
+    public function createProject(WP_REST_Request $request): WP_REST_Response
+    {
+        $id = $this->projects->create($request->get_json_params() ?: []);
+
+        return new WP_REST_Response([
+            'id' => $id,
+            'message' => 'Project created',
+        ], 201);
     }
 }
