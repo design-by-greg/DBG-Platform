d043945bee7f7ad45a1fa5e052897147ff19547c
design-by-greg <contact@designbygreg.fr>
2026-06-29 12:18:32 +0200

    Connect Asset routes to repository

--- apps/wordpress-plugin/src/API/Routes/AssetRoutes.php
+++ apps/wordpress-plugin/src/API/Routes/AssetRoutes.php
@@ -2,25 +2,49 @@
 
 namespace DBGPlatform\API\Routes;
 
+use DBGPlatform\Database\Repositories\AssetRepository;
 use WP_REST_Request;
 use WP_REST_Response;
 
 class AssetRoutes
 {
+    private AssetRepository $assets;
+
+    public function __construct()
+    {
+        $this->assets = new AssetRepository();
+    }
+
     public function register(): void
     {
         register_rest_route('dbg/v1', '/assets', [
-            'methods' => 'GET',
-            'callback' => [$this, 'listAssets'],
-            'permission_callback' => '__return_true',
+            [
+                'methods' => 'GET',
+                'callback' => [$this, 'listAssets'],
+                'permission_callback' => '__return_true',
+            ],
+            [
+                'methods' => 'POST',
+                'callback' => [$this, 'createAsset'],
+                'permission_callback' => '__return_true',
+            ],
         ]);
     }
 
     public function listAssets(WP_REST_Request $request): WP_REST_Response
     {
         return new WP_REST_Response([
-            'data' => [],
-            'message' => 'Asset API scaffold',
+            'data' => $this->assets->all(),
         ], 200);
+    }
+
+    public function createAsset(WP_REST_Request $request): WP_REST_Response
+    {
+        $id = $this->assets->create($request->get_json_params() ?: []);
+
+        return new WP_REST_Response([
+            'id' => $id,
+            'message' => 'Asset created',
+        ], 201);
     }
 }
